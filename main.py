981602ecc8a7c77ca2b340228ac6558791368d89
Your Name <you@example.com>
2024-11-22 16:22:01 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+nama = "satria"
+hobi = 'musik'
+umur = 20
+laki = True
+print("=====BIODATA=====")
+print("nama: %s" % (nama))
+print("hobi: %s, umur: %d, laki: %r" % (hobi,umur,laki))
---
+++ main2 kali.py
@@ -0,0 +1,8 @@
+nama: str = "satria"
+hobi: str = 'musik'
+umur: int = 20
+laki: bool = True
+nilai: float = 80.5
+print("nama: %s" % (nama))
+print("hobi: %s, umur: %d, laki: %r" % (hobi,umur,laki))
+print("rata-rata nilai: %f" % (80.5))
